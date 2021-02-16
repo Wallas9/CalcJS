2ec15ed71cfa552eff67459f542354b0fc693301
Wallas9 <wallas.rodolfo9@gmail.com>
2021-02-16 16:05:32 -0300

    Implementando as operações

--- calc.js
+++ calc.js
@@ -1,5 +1,55 @@
-const clicker = (btn) => {
-    let num = btn.value;
-    document.getElementById("result").value += num;
+
+const clicker = (element) => {
+    let result = document.getElementById("result");
+    if (result.value == "0" || result.value == "Syntax error") {
+        result.value = element.textContent;
+    }
+    else {
+        result.value += element.textContent;
+    }
+}
+
+const operation = (element) => {
+    let result = document.getElementById("result");
+    if (result.value == "0" || result.value == "Syntax error") {
+        result.value = "0";
+    }
+    else {
+        result.value += element.textContent;
+    }
+}
+
+const res = () => {
+    try {
+        let result = document.getElementById("result");
+        if (result.value != "") {
+            let calc = eval(result.value);
+            result.value = calc;
+        }
+    }
+    catch (err) {
+        result.value = "Syntax error";
+    }
+}
+
+const erase = () => {
+    let result = document.getElementById("result");
+    let resultNum = result.value;
+    result.value = resultNum.substring(0, resultNum.length - 1);
+}
+
+const cleaner = () => {
+    let result = document.getElementById("result");
+    result.value = "0";
+}
+
+const symbol = () => {
+    let result = document.getElementById("result");
+    if (result.value == "0" || result.value == "Syntax error") {
+        result.value = "0";
+    }
+    else {
+        result.value = result.value * -1;
+    }
 }
 
